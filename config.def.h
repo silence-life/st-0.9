2b25715fadbc74c4347eb08908e289e206b921fb
silence-life <silence_life@outlook.com>
2024-12-22 22:09:41 +0800

    step...

--- config.def.h
+++ config.def.h
@@ -180,8 +180,8 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},		0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},		0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 10},		0, /* !alt */ -1 },
+	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 10},		0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -210,8 +210,10 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_backslash,    copyurl,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_Up,     kscrollup,      {.i = 1} },
-	{ ShiftMask,            XK_Down,   kscrolldown,    {.i = 1} },
+	{ XK_ANY_MOD,           XK_Page_Up,     kscrollup,      {.i = 30} },
+	{ XK_ANY_MOD,           XK_Page_Down,   kscrolldown,    {.i = 30} },
+	{ ShiftMask,            XK_Up,     kscrollup,      {.i = 5} },
+	{ ShiftMask,            XK_Down,   kscrolldown,    {.i = 5} },
 };
 
 /*
